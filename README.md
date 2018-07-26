d6d401b48bc6d6f4302b203cf74ff6966f8a5a58
Owyn <Owyn@users.noreply.github.com>
2018-07-27 02:34:45 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# no-grim-strike-cancel
+# no_more_wasted_grim-strikes
 Stops early canceling of grim strike from other grim strikes.
+
+Makes it impossible to cast grim strike again before the 2nd hit (with biggest dmg) of previous one is made
+
+here are some debug calculations images from this module:
+(passed - means time passed in ms from the start of the cast of previous grim strike skill)
+(unlocked - means time passed in ms after which you can again safely cast grim strike preserving whole potential damage)
+<img src=http://u.cubeupload.com/Owyn/newgrim.jpg>
+
+and here is one from original old module:
+(last line was the time for when casting became available again - more than twice as long unnecessary)
+<img src=http://u.cubeupload.com/Owyn/oldgrim.jpg>
--- index.js
+++ index.js
@@ -1,46 +1,64 @@
-const GRIM_STRIKE = 5;
+const Command = require('command');
+const GRIM_STRIKE = 5; // 50300 & 50330
+const GRIM_TIMEOUT = 1400; // ms // in case you miss your target and there's no S_EACH_SKILL_RESULT, but you wanna recast it real fast lol
 
-module.exports = function NoGrimStrikeCancel(dispatch) {
-    let gameId = 0,
-    job = -1,
+module.exports = function NoWastedGrimStrikes(dispatch) {
+	const command = Command(dispatch);
+    let hooks = [],
+	gameId = 0,
     locked = false,
-    eventId = 0;
+	prevgrim = 0;
     
     dispatch.hook('S_LOGIN', 9, event => {
         gameId = event.gameId;
-        job = event.templateId % 100 - 1;
+		if (event.templateId % 100 - 1 === 8 && !hooks.length)
+		{
+			hook('C_START_SKILL', 6, event => {
+				if (locked && Math.floor(event.skill.id / 10000) === GRIM_STRIKE && Date.now() - prevgrim < GRIM_TIMEOUT)
+				{
+					return false;
+				}
+				if (Math.floor(event.skill.id / 10000) === GRIM_STRIKE) 
+				{
+					//command.message('<font color="#00FFFF">SS Locked1 </font>' + " prev: " + String(Date.now() - prevgrim));
+					locked = true;
+					prevgrim = Date.now();
+				}
+			});
+			
+			hook('S_EACH_SKILL_RESULT', 7, event => {
+				if(gameId.equals(event.source))
+				{
+					if (Math.floor(event.skill.id / 10000) === GRIM_STRIKE)
+					{
+						/*if(event.damage != 0)
+						{
+							command.message('<font color="#FBB917"> GRIM_DMG ' + String(event.stage) + ' </font>: '  + String(event.damage));
+						}*/
+						if (event.stage === 1) // when 2nd hit is done
+						{
+							locked = false;
+							//command.message("R UNlocked1 " + " passed: " + String(Date.now() - prevgrim));
+						} // stage 2 happens if you don't cancel animationa after stage 1
+					}
+				}
+			});
+		}
+		else
+		{
+			unload();
+		}
     })
-    
-    dispatch.hook('C_START_SKILL', 1, event => {
-        if (job === 8) {
-            const skill = Math.floor((event.skill - 0x4000000) / 10000);
-            if (locked && skill === GRIM_STRIKE) return false;
-            if (skill === GRIM_STRIKE) locked = true;
-        }
-    })
-    
-    dispatch.hook('S_ACTION_STAGE', 4, event => {
-        if(gameId.equals(event.gameId) && job === 8) {
-            const skill = Math.floor((event.skill - 0x4000000) / 10000)
-            
-            if (skill === GRIM_STRIKE ) {
-                if (event.stage === 0) {
-                    eventId = event.id;
-                    locked = true;
-                } else {
-                    locked = false;
-                }
-            }
-        }
-    })
-    
-    dispatch.hook('S_ACTION_END', 3, event => {
-        if(gameId.equals(event.gameId)) {
-            if (eventId === event.id) locked = false;
-        }
-    })
-    
-    dispatch.hook('C_CANCEL_SKILL', 1, event => {
-        if (job === 8) locked = false;
-    })
+	
+	function unload() {
+		if(hooks.length) {
+			for(let h of hooks) dispatch.unhook(h)
+
+			hooks = []
+		}
+	}
+
+	function hook() {
+		hooks.push(dispatch.hook(...arguments))
+	}
 }
