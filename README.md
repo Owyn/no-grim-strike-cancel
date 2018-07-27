1d8fd0fa2adbe57030d219852610f1fd064ff212
Owyn <Owyn@users.noreply.github.com>
2018-07-28 01:23:34 +0300

    Skill Prediction comp, + smoothness (ppl were complaining)

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # no_more_wasted_grim-strikes
 Stops early canceling of **grim strike** <img src=http://u.cubeupload.com/Owyn/GrimStrike1.png> from other grim strikes or sheer.
 
-Makes it impossible to cast grim strike again or **sheer** before the 2nd hit of grimstrike (with biggest dmg) has passed, or till 1.4sec elapsed in case you fully missed your enemy or was just hitting air for some reason (don't do that), doesn't stop sundering from early cancelling grimstrike though
+Delays casting of grim strike again or sheer before the 2nd hit of grimstrike (with biggest dmg) has passed, or till 1.4sec elapsed in case you fully missed your enemy or you was just hitting air for some reason (don't do that and expect something...), doesn't stop sundering from early cancelling grimstrike though, because you might want to hit it asap in some cases
 
-**warning** - it might not work fully if you use **Skill Prediction**, it's not like there's a way for both these modules to work together fine...
+**warning** - it waits for dmg number for 2nd hit to reach the client, meaning ping would delay it, but it should be compensated with smooth skill delaying implemented here
 
 here are some debug calculations images from this module:
 (passed - means time passed in ms from the start of the cast of previous grim strike skill)
--- index.js
+++ index.js
@@ -1,26 +1,67 @@
-const Command = require('command');
+//const Command = require('command');
 const GRIM_STRIKE = 5; // 50300 & 50330
 const GRIM_TIMEOUT = 1400; // ms // in case you miss your target and there's no S_EACH_SKILL_RESULT, but you wanna recast it real fast lol
 const SHEER = 3; // 30300 // sheer can cancel grim before it has done 2nd hit
+const CAT_GRIM = 90005;
+const CAT_SHEER =90003;
 
 module.exports = function NoWastedGrimStrikes(dispatch) {
-	const command = Command(dispatch);
+	//const command = Command(dispatch);
     let hooks = [],
+	queue = 0;
 	gameId = 0,
     locked = false,
 	prevgrim = 0;
-    
-    dispatch.hook('S_LOGIN', 10, event => {
-        gameId = event.gameId;
-		if (event.templateId % 100 - 1 === 8 && !hooks.length)
+	
+	function handleredcleanup(skill)
+	{
+		let qskill = Math.floor(queue.skill.id / 10000);
+		if(queue.skill == GRIM_STRIKE)
 		{
-			hook('C_START_SKILL', 6, {order: 200, filter: {fake: null}}, event => {
-				let skill = Math.floor(event.skill.id / 10000)
+			qskill = CAT_GRIM;
+		}
+		else
+		{
+			qskill = CAT_SHEER;
+		}
+		if(qskill != skill)
+		{
+			//dispatch.toClient('S_SKILL_CATEGORY', 3, {category: qskill, enabled: true});
+		}
+	}
+	
+    dispatch.hook('S_LOGIN', 10, ev => {
+        gameId = ev.gameId;
+		if (ev.templateId % 100 - 1 === 8 && !hooks.length)
+		{
+			hook('C_START_SKILL', 6, {order: -20, filter: {fake: null}}, event => {
+				let skill = Math.floor(event.skill.id / 10000);
 				if (locked && (skill === GRIM_STRIKE || skill === SHEER) && Date.now() - prevgrim < GRIM_TIMEOUT)
 				{
+					if(queue)
+					{
+						handleredcleanup(skill);
+					}
+					queue = event;
+					//command.message('<font color="#C0C0C0">' + String(skill)+ ' queued </font>');
+					if(skill == GRIM_STRIKE)
+					{
+						skill = CAT_GRIM;
+					}
+					else
+					{
+						skill = CAT_SHEER;
+					}
+					//dispatch.toClient('S_SKILL_CATEGORY', 3, {category: skill, enabled: false});
 					return false;
 				}
-				if (Math.floor(event.skill.id / 10000) === GRIM_STRIKE) 
+				else if(queue)
+				{
+					handleredcleanup(0);
+					queue = 0;
+					//command.message('queue<font color="#A52A2A"> cleared </font>');
+				}
+				if (skill === GRIM_STRIKE) 
 				{
 					//command.message('<font color="#00FFFF">SS Locked1 </font>' + " prev: " + String(Date.now() - prevgrim));
 					locked = true;
@@ -40,7 +81,21 @@ module.exports = function NoWastedGrimStrikes(dispatch) {
 						if (event.stage === 1) // when 2nd hit is done
 						{
 							locked = false;
-							//command.message("R UNlocked1 " + " passed: " + String(Date.now() - prevgrim));
+							//command.message('<font color="#CD0000"> R UNlocked1</font> ' + " passed: " + String(Date.now() - prevgrim));
+							if(queue)
+							{
+								let skill = Math.floor(queue.skill.id / 10000);
+								dispatch.toServer('C_START_SKILL', 6, queue);
+								if(skill == GRIM_STRIKE)
+								{
+									skill = CAT_GRIM;
+								}
+								else
+								{
+									skill = CAT_SHEER;
+								}
+								//dispatch.toClient('S_SKILL_CATEGORY', 3, {category: skill, enabled: true});
+							}
 						} // stage 2 happens if you don't cancel animationa after stage 1
 					}
 				}
