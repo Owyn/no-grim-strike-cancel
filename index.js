5af8556e7f5f5cf1980ee7f2f3c7b07736a13b3c
Owyn <Owyn@users.noreply.github.com>
2018-09-13 22:12:55 +0300

    fix reloging from reaper to reaper (why u'd do that?)

--- index.js
+++ index.js
@@ -27,92 +27,95 @@ module.exports = function NoWastedGrimStrikes(dispatch) {
 	
     dispatch.hook('S_LOGIN', 10, ev => {
         gameId = ev.gameId;
-		if (ev.templateId % 100 - 1 === 8 && !hooks.length)
+		if (ev.templateId % 100 - 1 === 8) // it's a reaper
 		{
-			dispatch.hook('S_CREST_MESSAGE', 2, event => { // needed only for SP compatibility
-				if (event.type === 6 && Math.floor(event.skill / 10000) === GRIM_STRIKE)
-				{
-					//command.message('reset <font color="#A52A2A">'+ event.skill +' </font>');
-					canrecast = true;
-				}
-			});
-
-			hook('C_START_SKILL', dispatch.majorPatchVersion >= 74 ? 7 : 6, {order: -200}, event => {
-				let skill = Math.floor(event.skill.id / 10000);
-				if (locked && (skill === GRIM_STRIKE || skill === SHEAR || skill === SUNDERING) && Date.now() - prevgrim < GRIM_TIMEOUT)
-				{
-					//command.message('<font color="#C0C0C0">' + String(skill)+ ' queued </font>');
-					if(skill !== isred && haspretty)
+			if(!hooks.length) // user could relog from his reaper to his another reaper...
+			{
+				hook('S_CREST_MESSAGE', 2, event => { // needed only for SP compatibility
+					if (event.type === 6 && Math.floor(event.skill / 10000) === GRIM_STRIKE)
 					{
-						if(queue) // queued another skill while previous one wasn't casted yet - replacing
-						{
-							makenotred();
-						}
-						dispatch.toClient('S_SKILL_CATEGORY', 3, {category: skill+CAT_BASE, enabled: false});
-						isred = skill;
+						//command.message('reset <font color="#A52A2A">'+ event.skill +' </font>');
+						canrecast = true;
 					}
-					queue = event;
-					return false;
-				}
-				else if(queue)
-				{
-					queue = 0;
-					//command.message('queue<font color="#A52A2A"> cleared </font>');
-				}
-				if (skill === GRIM_STRIKE) 
-				{
-					//command.message('<font color="#00FFFF">SS Locked1 </font>' + " prev: " + String(Date.now() - prevgrim));
-					locked = true;
-					canrecast = false;
-					prevgrim = Date.now();
-				}
-				else if (skill !== SHEAR && skill !== SUNDERING) // you missed, didn't you?
-				{
-					makenotred();
-				}
-			});
-			
-			hook('S_EACH_SKILL_RESULT', dispatch.majorPatchVersion >= 74 ? 11 : 10, event => {
-				if(gameId.equals(event.source))
-				{
-					/*if(event.damage != 0)
-					{
-						command.message('<font color="#FBB917"> GRIM_DMG ' + String(event.stage) + ' </font>: '  + String(event.damage));
-					}*/
+				});
+
+				hook('C_START_SKILL', 7, {order: -200}, event => {
 					let skill = Math.floor(event.skill.id / 10000);
-					if(event.stage === 0 && (skill === GRIM_STRIKE || skill === SHEAR || skill === SUNDERING))
+					if (locked && (skill === GRIM_STRIKE || skill === SHEAR || skill === SUNDERING) && Date.now() - prevgrim < GRIM_TIMEOUT)
+					{
+						//command.message('<font color="#C0C0C0">' + String(skill)+ ' queued </font>');
+						if(skill !== isred && haspretty)
+						{
+							if(queue) // queued another skill while previous one wasn't casted yet - replacing
+							{
+								makenotred();
+							}
+							dispatch.toClient('S_SKILL_CATEGORY', 3, {category: skill+CAT_BASE, enabled: false});
+							isred = skill;
+						}
+						queue = event;
+						return false;
+					}
+					else if(queue)
+					{
+						queue = 0;
+						//command.message('queue<font color="#A52A2A"> cleared </font>');
+					}
+					if (skill === GRIM_STRIKE) 
+					{
+						//command.message('<font color="#00FFFF">SS Locked1 </font>' + " prev: " + String(Date.now() - prevgrim));
+						locked = true;
+						canrecast = false;
+						prevgrim = Date.now();
+					}
+					else if (skill !== SHEAR && skill !== SUNDERING) // you missed, didn't you?
 					{
 						makenotred();
 					}
-					else if(event.stage === 1 && skill === GRIM_STRIKE) // when 2nd hit is done
+				});
+				
+				hook('S_EACH_SKILL_RESULT', 12, event => {
+					if(gameId.equals(event.source))
 					{
-						locked = false;
-						//command.message('<font color="#CD0000"> R UNlocked1</font> ' + " passed: " + String(Date.now() - prevgrim));
-						if(queue)
+						/*if(event.damage != 0)
 						{
-							if(Math.floor(queue.skill.id / 10000) === GRIM_STRIKE)
+							command.message('<font color="#FBB917"> GRIM_DMG ' + String(event.stage) + ' </font>: '  + String(event.damage));
+						}*/
+						let skill = Math.floor(event.skill.id / 10000);
+						if(event.stage === 0 && (skill === GRIM_STRIKE || skill === SHEAR || skill === SUNDERING))
+						{
+							makenotred();
+						}
+						else if(event.stage === 1 && skill === GRIM_STRIKE) // when 2nd hit is done
+						{
+							locked = false;
+							//command.message('<font color="#CD0000"> R UNlocked1</font> ' + " passed: " + String(Date.now() - prevgrim));
+							if(queue)
 							{
+								if(Math.floor(queue.skill.id / 10000) === GRIM_STRIKE)
+								{
+									if(canrecast)
+									{
+										locked = true;
+										prevgrim = Date.now();
+									}
+								}
+								else
+								{
+									canrecast = true;
+								}
+								//command.message('can recast: ' + String(canrecast));
 								if(canrecast)
 								{
-									locked = true;
-									prevgrim = Date.now();
+									canrecast = false;
+									dispatch.toServer('C_START_SKILL', 7, queue);
+									queue = 0;
 								}
 							}
-							else
-							{
-								canrecast = true;
-							}
-							//command.message('can recast: ' + String(canrecast));
-							if(canrecast)
-							{
-								canrecast = false;
-								dispatch.toServer('C_START_SKILL', dispatch.majorPatchVersion >= 74 ? 7 : 6, queue);
-								queue = 0;
-							}
-						}
-					} // stage 2 happens if you don't cancel animation after stage 1
-				}
-			});
+						} // stage 2 happens if you don't cancel animation after stage 1
+					}
+				});
+			}
 		}
 		else
 		{
